08b668b4f1759f448728fc961e9effc19453d4b6
Ajay <lci2021023@iiitl.ac.in>
2023-03-13 13:28:42 +0530

    Home Img fix + minors

--- js/expiring.js
+++ js/expiring.js
@@ -1,91 +1,91 @@
-// console.log("Expiring patents");
+// // console.log("Expiring patents");
 
-const a = 
-{
-     "PREJATA":[ "PREJATA", "$4.73 Billion (2021)", "US7618631B2" , "April 17, 2022", "US8337856B2" , "April 18, 2022", "US7862817B2" , "April 19, 2022", "US7097840B2" , "April 20, 2022"], 
-     "STELARA":[ "STELARA", "$9.13 Billion (2021)", "US9409984B2" , "January 13,2022", "US8337856B2" , "February 27, 2022", "US6902734B2" , "May 3, 2022", "US7097840B2" , "September 25, 2023"], 
-}
+// const a = 
+// {
+//      "PREJATA":[ "PREJATA", "$4.73 Billion (2021)", "US7618631B2" , "April 17, 2022", "US8337856B2" , "April 18, 2022", "US7862817B2" , "April 19, 2022", "US7097840B2" , "April 20, 2022"], 
+//      "STELARA":[ "STELARA", "$9.13 Billion (2021)", "US9409984B2" , "January 13,2022", "US8337856B2" , "February 27, 2022", "US6902734B2" , "May 3, 2022", "US7097840B2" , "September 25, 2023"], 
+// }
 
-console.log(a[0]);
+// console.log(a[0]);
 
-let outerdiv = document.getElementById("expiring");
-let innerdiv = document.getElementById("here4");
-let worth = document.getElementById("comworth");
-let select = document.querySelector('select')
-let patent1,patent2,patent3;
+// let outerdiv = document.getElementById("expiring");
+// let innerdiv = document.getElementById("here4");
+// let worth = document.getElementById("comworth");
+// let select = document.querySelector('select')
+// let patent1,patent2,patent3;
 
-select.addEventListener('change',()=>{
-  cur_selected = select.value;
-//   console.log(patent1);
-  console.log(a[patent1]);
-  let html = ``;
-  html+=`  
-  <div class="container" style="display: flex; margin:4vw">
-    <div class="row row-cols-1 row-cols-md-2 row-cols-xl-4">
-           <div class="col">
-             <div class="card5 radius-10 border-start border-0 border-3 border-info">
-                <div class="card-body">
-                    <div class="d-flex align-items-center">
-                        <div>
-                            <h4 class="my-1 text-info">${a[cur_selected][2]}</h4>
-                        </div>
-                        <div class="widgets-icons-2 rounded-circle bg-gradient-scooter text-white ms-auto"><i class="fa fa-shopping-cart"></i>
-                        </div>
-                    </div>
-                        <h4  class="textinside" style="margin-top: 6vh;">Expiration-Date: ${a[cur_selected][3]}</h4>
-                </div>
-             </div>
-           </div>
-           <div class="col">
-            <div class="card5 radius-10 border-start border-0 border-3 border-danger">
-               <div class="card-body">
-                   <div class="d-flex align-items-center">
-                       <div>
-                           <h4 class="my-1 text-danger">${a[cur_selected][4]}</h4>
-                        </div>
-                        <div class="widgets-icons-2 rounded-circle bg-gradient-bloody text-white ms-auto"><i class="fa fa-dollar"></i>
-                        </div>
-                    </div>
-                    <h4  class="textinside" style="margin-top: 6vh;">Expiration-Date: ${a[cur_selected][5]}</h4>
-               </div>
-            </div>
-          </div>
-          <div class="col">
-            <div class="card5 radius-10 border-start border-0 border-3 border-success">
-               <div class="card-body">
-                   <div class="d-flex align-items-center">
-                       <div>
-                           <h4 class="my-1 text-success">${a[cur_selected][6]}</h4>
-                       </div>
-                       <div class="widgets-icons-2 rounded-circle bg-gradient-ohhappiness text-white ms-auto"><i class="fa fa-bar-chart"></i>
-                       </div>
-                   </div>
-                   <h4  class="textinside" style="margin-top: 6vh;">Expiration-Date: ${a[cur_selected][7]}</h4>
-               </div>
-            </div>
-          </div>
-          <div class="col">
-            <div class="card5 radius-10 border-start border-0 border-3 border-warning">
-               <div class="card-body">
-                   <div class="d-flex align-items-center">
-                       <div>
-                           <h4 class="my-1 text-warning">${a[cur_selected][8]}</h4>
-                       </div>
-                       <div class="widgets-icons-2 rounded-circle bg-gradient-blooker text-white ms-auto"><i class="fa fa-users"></i>
-                       </div>
-                   </div>
-                   <h4  class="textinside" style="margin-top: 6vh;">Expiration-Date: ${a[cur_selected][9]}</h4>
-               </div>
-            </div>
-          </div> 
-        </div>
-    </div>
+// select.addEventListener('change',()=>{
+//   cur_selected = select.value;
+// //   console.log(patent1);
+//   console.log(a[patent1]);
+//   let html = ``;
+//   html+=`  
+//   <div class="container" style="display: flex; margin:4vw">
+//     <div class="row row-cols-1 row-cols-md-2 row-cols-xl-4">
+//            <div class="col">
+//              <div class="card5 radius-10 border-start border-0 border-3 border-info">
+//                 <div class="card-body">
+//                     <div class="d-flex align-items-center">
+//                         <div>
+//                             <h4 class="my-1 text-info">${a[cur_selected][2]}</h4>
+//                         </div>
+//                         <div class="widgets-icons-2 rounded-circle bg-gradient-scooter text-white ms-auto"><i class="fa fa-shopping-cart"></i>
+//                         </div>
+//                     </div>
+//                         <h4  class="textinside" style="margin-top: 6vh;">Expiration-Date: ${a[cur_selected][3]}</h4>
+//                 </div>
+//              </div>
+//            </div>
+//            <div class="col">
+//             <div class="card5 radius-10 border-start border-0 border-3 border-danger">
+//                <div class="card-body">
+//                    <div class="d-flex align-items-center">
+//                        <div>
+//                            <h4 class="my-1 text-danger">${a[cur_selected][4]}</h4>
+//                         </div>
+//                         <div class="widgets-icons-2 rounded-circle bg-gradient-bloody text-white ms-auto"><i class="fa fa-dollar"></i>
+//                         </div>
+//                     </div>
+//                     <h4  class="textinside" style="margin-top: 6vh;">Expiration-Date: ${a[cur_selected][5]}</h4>
+//                </div>
+//             </div>
+//           </div>
+//           <div class="col">
+//             <div class="card5 radius-10 border-start border-0 border-3 border-success">
+//                <div class="card-body">
+//                    <div class="d-flex align-items-center">
+//                        <div>
+//                            <h4 class="my-1 text-success">${a[cur_selected][6]}</h4>
+//                        </div>
+//                        <div class="widgets-icons-2 rounded-circle bg-gradient-ohhappiness text-white ms-auto"><i class="fa fa-bar-chart"></i>
+//                        </div>
+//                    </div>
+//                    <h4  class="textinside" style="margin-top: 6vh;">Expiration-Date: ${a[cur_selected][7]}</h4>
+//                </div>
+//             </div>
+//           </div>
+//           <div class="col">
+//             <div class="card5 radius-10 border-start border-0 border-3 border-warning">
+//                <div class="card-body">
+//                    <div class="d-flex align-items-center">
+//                        <div>
+//                            <h4 class="my-1 text-warning">${a[cur_selected][8]}</h4>
+//                        </div>
+//                        <div class="widgets-icons-2 rounded-circle bg-gradient-blooker text-white ms-auto"><i class="fa fa-users"></i>
+//                        </div>
+//                    </div>
+//                    <h4  class="textinside" style="margin-top: 6vh;">Expiration-Date: ${a[cur_selected][9]}</h4>
+//                </div>
+//             </div>
+//           </div> 
+//         </div>
+//     </div>
 
-`
+// `
 
-worth.innerText = a[cur_selected][1];
-innerdiv.innerHTML = html;
-})
+// worth.innerText = a[cur_selected][1];
+// innerdiv.innerHTML = html;
+// })
 
 
 
